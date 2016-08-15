b325a5859473fb03aba740333114845573883d3c
kk00ss <acc4konstantin@gmail.com>
2016-08-15 13:35:56 +0300

    initial commit

---
+++ ConsoleApplication3/Definitions.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Collections.Concurrent;
+
+namespace ConsoleApplication3
+{
+    class Match<T1, T2>
+    {
+        private T1 input { get; }
+        private T2 result;
+        protected bool isMatched = false;
+        protected bool isComputed = false;
+        public Match(T1 what, bool isMatched = false)
+        {
+            this.input = what;
+            this.isMatched = isMatched;
+        }
+        public CaseClause Case(Func<T1, bool> test)
+        {
+            if (!this.isMatched && test(this.input))
+                this.isMatched = true;
+            return new CaseClause(this);
+        }
+        public Match<T1, T2> CaseDo<SubType>(Func<SubType, T2> action) {
+            var subtype = typeof(SubType);
+            if (subtype.IsInstanceOfType(input))
+            {
+                isMatched = true;
+                result = action((SubType)Convert.ChangeType(input, subtype));
+                isComputed = true;
+            }
+            return this;
+        }
+        public T2 Default(Func<T1, T2> action)
+        {
+            if (this.isMatched && !this.isComputed)
+            {
+                this.result = action(input);
+                this.isComputed = true;
+            }
+            return this.result;
+        }
+        public class CaseClause
+        {
+            private Match<T1, T2> parent;
+            public CaseClause(Match<T1,T2> parent)
+            {
+                this.parent = parent;
+            }
+            public Match<T1, T2> Do(Func<T1, T2> action)
+            {
+                if (parent.isMatched && !parent.isComputed) {
+                    parent.result = action(parent.input);
+                    parent.isComputed = true;
+                }
+                return parent;
+            }
+        }
+    }
+
+    public class Either<T1, T2>
+    {
+        private bool leftOrRight; // false for left, true for right
+        public bool isLeft { get { return !leftOrRight; } }
+        public bool isRight { get { return leftOrRight; } }
+        public T1 Left { get; }
+        public T2 Right { get; }
+        public Either(T1 left)
+        {
+            leftOrRight = false;
+            Left = left;
+        }
+        public Either(T2 right)
+        {
+            leftOrRight = true;
+            Right = right;
+        }
+    }
+
+    public static class FunctionalExtensions
+    {
+        public static ICollection<T> flattenOpt<T>(this ICollection<Option<T>> collection)
+        {
+            var type = collection.GetType();
+            var type2 = type.GetGenericTypeDefinition();
+            var type3 = type2.MakeGenericType(typeof(T));
+            ICollection<T> ret = (ICollection<T>) Activator.CreateInstance(type3);
+            foreach (var el in collection)
+                if (el.isDefinded)
+                    ret.Add(el.get);
+            return ret;
+        }
+    }
+
+    public abstract class Option<T>
+    {
+        public virtual Boolean isDefinded { get; }
+        public virtual Boolean isEmpty { get; }
+        public abstract T get { get; }
+        public abstract Option<T1> Map<T1>(Func<T, T1> action);
+    }
+
+    public class None<T> : Option<T>
+    {
+        public override Boolean isDefinded { get { return false; } }
+        public override Boolean isEmpty { get { return true; } }
+        public override Option<T1> Map<T1>(Func<T, T1> action) { return new None<T1>(); }
+        public override T get
+        {
+            get
+            {
+                throw new Exception("No element, option is not defined");
+            }
+        }
+    }
+
+    public class Some<T> : Option<T>
+    {
+        public override Boolean isDefinded { get { return true; } }
+        public override Boolean isEmpty { get { return false; } }
+        private T data { get; }
+        public Some(T data)
+        {
+            if (data == null)
+                throw new ArgumentNullException();
+            this.data = data;
+        }
+        public override Option<T1> Map<T1>(Func<T, T1> action)
+        {
+            var res = action(this.data);
+            return new Some<T1>(res);
+        }
+        public override T get
+        {
+            get
+            {
+                return this.data;
+            }
+        }
+    }
+
+    public class Try<T>
+    {
+        public Boolean isSuccess { get; }
+        public Boolean isFailure { get; }
+        private T result;
+        private Exception ex;
+
+        public Try(Func<T> action)
+        {
+            try
+            {
+                this.result = action();
+                isSuccess = true;
+                isFailure = false;
+            }
+            catch (Exception ex)
+            {
+                this.ex = ex;
+                isSuccess = false;
+                isFailure = true;
+            }
+        }
+
+        public T get()
+        {
+            if (isSuccess)
+                return result;
+            else throw new ArgumentException("Try failed, cannot get result");
+        }
+
+        public Exception exception()
+        {
+            if (isFailure)
+                return this.ex;
+            else throw new ArgumentException("Try succeded, cannot get exception");
+        }
+
+        public Try<T1> OnSuccess<T1>(Func<T, T1> action)
+        {
+            Func<T1> func = () => action(this.result);
+            return new Try<T1>(func);
+        }
+
+        public Try<T1> OnFailure<T1>(Func<Exception, T1> action)
+        {
+            Func<T1> func = () => action(this.ex);
+            return new Try<T1>(func);
+        }
+    }
+
+    public class Immutable<T> // where T : new()
+    {
+        private static ConcurrentDictionary<string, PropertyInfo[]> properitesCache 
+            = new ConcurrentDictionary<string, PropertyInfo[]>();
+
+        public Immutable()
+        {
+            var thisType = this.GetType();
+            PropertyInfo[] props = null;
+            properitesCache.TryGetValue(thisType.FullName, out props);
+            if (props == null)
+            {
+                var constructors = thisType.GetConstructors();
+                var largestConstructorParams = constructors
+                    .Select(x => x.GetParameters())
+                    .OrderByDescending(x => x.Length)
+                    .First();
+                props = thisType.GetProperties();
+                properitesCache[thisType.FullName] = props;
+                for (var i = 0; i < props.Length; i++)
+                {
+                    var p = props[i];
+                    if (!p.CanRead || p.CanWrite)
+                        throw new FieldAccessException(
+                                "Properties in CaseClasses should not have setter property name: " +
+                                p.Name + " of type: " + p.PropertyType + " in class: " + thisType.FullName);
+                    if (p.PropertyType != largestConstructorParams[i].ParameterType)
+                        throw new ArgumentException("Constructor parameters for immutable type should have same types as class properties, propertyType: " + p.PropertyType +
+                            " propertyName " + p.Name);
+                    if (p.Name != largestConstructorParams[i].Name)
+                        throw new ArgumentException("Constructor parameters for immutable type should have same names as class properties, propertyType: " + p.PropertyType +
+                            " propertyName: " + p.Name);
+
+                }
+            }
+        }
+
+        public bool Equals(T that)
+        {
+            var ret = true;
+            foreach (var prop in properitesCache[this.GetType().FullName])
+                if (prop.GetValue(this) != prop.GetValue(that))
+                {
+                    ret = false;
+                    break;
+                }
+            return ret;
+        }
+
+        public override bool Equals(object that)
+        {
+            if(that.GetType() != this.GetType())
+                throw new InvalidOperationException("Don't use Equals with objects of other types");
+            return this.Equals((T)that);
+        }
+
+        public override int GetHashCode()
+        {
+            var ret = 0;
+            foreach (var prop in properitesCache[this.GetType().FullName])
+                ret += prop.GetValue(this).GetHashCode();
+            return ret;
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new A("1", "1");
+            var a2 = new A("1", "1");
+            var ret = a1.Equals(a2);
+            var ret2 = a1.GetHashCode() == a2.GetHashCode();
+            var tryres1 = new Try<int>(() =>
+            {
+                return 1;
+            });
+            var tryres2 = new Try<int>(() =>
+            {
+                throw new Exception("test");
+                return 1;
+            });
+            var e1 = new Either<int, string>("string");
+            var t1 = -1;
+            var t2 = "";
+            if (e1.isLeft)
+                t1 = e1.Left;
+            else t2 = e1.Right;
+
+            var t3 = new List<Option<string>>() {
+                new Some<string>("StringOption"),
+                new None<string>() ,
+                new Some<string>("Bla"),
+                new None<string>() };
+            var t4 = t3.Select(x => x.Map<int>(y => y.Length));
+            foreach (var opt in t4)
+            {
+                Console.Write(opt.isDefinded + " ");
+                if (opt.isDefinded)
+                    Console.Write(opt.get);
+                Console.WriteLine();
+            }
+            var t5 = t4.Where(x => x.isDefinded);
+            var t6 = new Match<string, int>("abcdef")
+                    .Case(x => x.Length == 10).Do(x => 10)
+                    .Case(x => x.Length == 6).Do(x => 6)
+                    .Default(x => -1);
+
+            var t7 = t3.flattenOpt();
+            var t8 = new Match<object, Boolean>((object)a1)
+                .CaseDo<A>(x => {
+                    Console.WriteLine(x.Name);
+                    return true;
+                }).Default(x => false);
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+    class A : Immutable<A>
+    {
+        public string Name { get; }
+        public string LastName { get; }
+
+        public A()
+        {
+            throw new Exception("don't use empty constructor of immutable class");
+        }
+        public A(string Name, string LastName)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+        }
+    }
+}
